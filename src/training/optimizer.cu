104ee1622b73ac061bf04f734d1cba6506d8c4bc
ChristianPE1 <cpardave@unsa.edu.pe>
2025-06-21 22:30:48 -0500

    Add: function to update LR

--- src/training/optimizer.cu
+++ src/training/optimizer.cu
@@ -11,21 +11,9 @@ __global__ void updateWeightsKernel(float *weights, float *gradients, float lear
 
 Optimizer::Optimizer(float learning_rate) : learning_rate(learning_rate) {}
 
-void Optimizer::update(float *weights, float *gradients, int size) {
-    float *d_weights, *d_gradients;
-
-    cudaMalloc(&d_weights, size * sizeof(float));
-    cudaMalloc(&d_gradients, size * sizeof(float));
-
-    cudaMemcpy(d_weights, weights, size * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_gradients, gradients, size * sizeof(float), cudaMemcpyHostToDevice);
-
+void SGD::step(float* params, float* grads, size_t size) {
     int blockSize = 256;
     int numBlocks = (size + blockSize - 1) / blockSize;
-    updateWeightsKernel<<<numBlocks, blockSize>>>(d_weights, d_gradients, learning_rate, size);
-
-    cudaMemcpy(weights, d_weights, size * sizeof(float), cudaMemcpyDeviceToHost);
-
-    cudaFree(d_weights);
-    cudaFree(d_gradients);
+    updateWeightsKernel<<<numBlocks, blockSize>>>(params, grads, learning_rate, size);
+    cudaDeviceSynchronize();
 }
